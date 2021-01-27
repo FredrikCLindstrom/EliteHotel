d76027f4e2f6397ba7d863ab88364ac8e576fa07
Aleluni <alexander_lunin@hotmail.com>
2021-01-27 16:28:55 +0100

    Alexander new master

--- src/Main/HotelManagementSystem.java
+++ src/Main/HotelManagementSystem.java
@@ -7,6 +7,7 @@ public class HotelManagementSystem {
     
     public static void main(String[] args) {
         System.out.println("Hej");
+        System.out.println("Hej2");
         
     }
     
