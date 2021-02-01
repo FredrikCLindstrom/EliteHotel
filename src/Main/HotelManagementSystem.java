bbddd84bb05309225eb0f2d70b657a0249986c6f
lindstrom <lindstrom@localhost>
2021-02-01 15:00:32 +0100

    description av olika rumssorteenra

--- src/Main/HotelManagementSystem.java
+++ src/Main/HotelManagementSystem.java
@@ -228,13 +228,13 @@ public class HotelManagementSystem {
         for (Room room : testingRoomList) {
             i++;
             if(i%2==0){
-                room.guest=new Guest(firstNames[i],lastNames[i]);
+                room.guest=new Guest(firstNames[i],lastNames[i],2);
             }
         }
         testingRoomList.stream().forEach(e->System.out.println(e.toString()));
         System.out.println("test for occupied rooms");
         testingRoomList.stream().filter(e->e.guest!=null).forEach(e->System.err.println("Room Number:"+e.roomNr+" "+e.name+" is occupied"));
-        FileManagement.printToTextDoc(testingRoomList.get(3).getGuest());
+        //FileManagement.printToTextDoc(testingRoomList.get(3).getGuest());
         
     }
     
