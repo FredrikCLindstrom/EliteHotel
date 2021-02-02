3bf59dcd9a41c554fd166eda822ce461ccf4fb5d
lindstrom <lindstrom@localhost>
2021-02-02 11:42:40 +0100

    ny klass, 3 food totalt, lägga till fler?

---
+++ src/Main/Sandwich.java
@@ -0,0 +1,13 @@
+
+package Main;
+
+
+public class Sandwich extends Food{
+    
+    public Sandwich(int roomNr) {
+        super(roomNr);
+        this.name = "Sandwich";
+        this.cost = 100;
+    }
+    
+}
